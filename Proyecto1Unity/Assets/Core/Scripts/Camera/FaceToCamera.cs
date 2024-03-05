0d11ff6cd022a825cb565fdccd541cda0ead8024
Pau <psb1005@alu.ubu.es>
2024-03-05 13:51:55 +0100

    :sparkles: Camera and movement system implemented
    · Added Cinemachine camera (still needs working on the views).
    · Added movement system (walk, run, jump).
    · Added camera controls.
    · Added Input System (keyboard + mouse and controller).
    · Jump control on press added.
    · Coyote Time added.
    · Jump Input Buffer added.
    · Sprite orientation script added.
    · Shadow under character added.

---
+++ Proyecto1Unity/Assets/Core/Scripts/Camera/FaceToCamera.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceToCamera : MonoBehaviour
+{
+    private Camera _camera;
+
+    private void Start()
+    {
+        _camera = FindFirstObjectByType<Camera>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.LookAt(_camera.transform.position);
+    }
+}
