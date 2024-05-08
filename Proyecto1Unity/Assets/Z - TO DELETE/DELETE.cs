7308265a5a549298e3c33e90f77cb91d17d7b275
PekeDani <dac1007@alu.ubu.es>
2024-05-08 03:58:57 +0200

    :globe_with_meridians: Blockout metrics adjustement
    -Changed some models and metrics in order to export the blockout models

--- Proyecto1Unity/Assets/Z - TO DELETE/DELETE.cs
+++ Proyecto1Unity/Assets/Z - TO DELETE/DELETE.cs
@@ -7,6 +7,7 @@ public class DELETE : MonoBehaviour
 {
     private bool slowTime = false;
     [SerializeField] Animator animator;
+    [SerializeField] int currentScene;
 
     private void Start()
     {
@@ -22,7 +23,7 @@ public class DELETE : MonoBehaviour
         
         if(Input.GetKeyDown(KeyCode.R))
         {
-            SceneManager.LoadScene(0);
+            SceneManager.LoadScene(currentScene);
         }
 
         if(Input.GetKeyDown(KeyCode.P))
